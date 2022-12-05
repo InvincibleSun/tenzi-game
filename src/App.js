2a5310e7185203954fe43fa1f2e28bda008c0a3b
InvincibleSun <103495967+InvincibleSun@users.noreply.github.com>
2022-12-05 21:40:20 +0400

    created a template App component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <main className="App">hello</main>;
+  return (
+    <main>
+      <div className="container">
+        <div className="content">
+          <h1>Tenzies</h1>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
